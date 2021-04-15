247b777c6b7400b2a4403367d0e2c194daf20c37
Oriana Sanabria <orianasanabria2@gmail.com>
2021-04-15 14:26:45 -0400

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Characters />
+  </div>
+</template>
+
+<script>
+import Characters from "@/components/Characters.vue";
+
+export default {
+  name: "App",
+  components: {
+    Characters,
+  },
+};
+</script>
+
+<style lang="scss">
+html,
+body {
+  box-sizing: border-box;
+  width: 100%;
+  background: black
+    url("https://rickandmortypod.com/wp-content/uploads/2018/11/cropped-RM_page-header_background1-3.png")
+    center center no-repeat;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: white;
+}
+</style>
---
+++ src/components/Characters.vue
@@ -0,0 +1,58 @@
+<template>
+  <div v-if="characters.length > 0" id="characters" class="container mt-5">
+    <div class="card-group">
+      <div class="row">
+        <div
+          v-for="(character, i) in characters"
+          :key="i"
+          class="card col-lg-6 col-12 mb-3 px-0 bg-dark"
+        >
+          <div class="row g-4">
+            <div class="col-md-4">
+              <img :src="character.image" alt="Character Image" width="100%" />
+            </div>
+            <div class="col-md-8 d-flex align-items-center">
+              <div class="card-body">
+                <h3 class="card-title text-light">{{ character.name }}</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Characters",
+  data() {
+    return {
+      characters: [],
+    };
+  },
+  methods: {
+    async getData() {
+      const url = "https://rickandmortyapi.com/api/character";
+      try {
+        const req = await axios(url);
+        if (!req) return;
+        this.characters = req.data.results;
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  created() {
+    this.getData();
+  },
+};
+</script>
+
+<style>
+.container {
+  box-sizing: border-box;
+  color: #000;
+}
+</style>
